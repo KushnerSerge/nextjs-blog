1f261a2580992e3f0e5aa1afbb8541074c20726e
Sergiu Cusnir <sergiu.cusnir@avanquest.com>
2022-07-05 19:51:49 +0300

    some changes in the new branch 1

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
-// we made some changes here
+// we made some changes here!
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
